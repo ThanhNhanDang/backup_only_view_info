f87d39ebe2e2d25a85327ce946f6a701b4d2e098
ThanhNhanDang <yesthanhnhan16@gmail.com>
2025-11-04 13:37:51 +0700

    update

--- flask_server.py
+++ flask_server.py
@@ -72,6 +72,8 @@ def index():
     files = []
     for file_name in os.listdir(BACKUP_DIR):
         file_path = os.path.join(BACKUP_DIR, file_name)
+        print(file_path)
+
         if os.path.isfile(file_path) and (file_name.endswith('.dump') or file_name.endswith('.zip')):
             file_size_mb = os.path.getsize(file_path) / (1024 * 1024)
             file_creation_time = os.path.getctime(file_path)
@@ -85,7 +87,6 @@ def index():
         0 if f['name'].endswith('.dump') else 1,
         -f['creation_time']
     ))
-    print(files)
 
     now = datetime.now()
     midnight = datetime(now.year, now.month, now.day) + timedelta(days=1)
